f328247d7b805bccfbe9b2a0754ba660d448184b
a8919779 <59424921+a8919779@users.noreply.github.com>
2021-02-03 17:09:51 +0900

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,4 +4,16 @@
 결과적으로 사이클을 돌때마다 큰숫자가 가장 끝으로 이동한다
 */
 public class BubbleSort {
+ int[] arr = new int[];
+ int temp = 0;
+
+ for (int i=0; i<arr.length; i++) {
+  for (int j=0; j<(arr.length-i-1); j++) {
+   if (arr[j] > arr[j+1]) {
+    temp = arr[j];
+    arr[j] = arr[j+1];
+    arr[j+1] = temp;
+   }
+  }
+ }
 }
