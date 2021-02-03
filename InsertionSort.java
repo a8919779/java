9614f26c22fce39cb59c60a2cdfdfb5dbd677f49
a8919779 <59424921+a8919779@users.noreply.github.com>
2021-02-04 07:49:21 +0900

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,6 +8,7 @@ public class InsertionSort {
   int min = 0;
   int max = 0;
   int temp = 0;
+  int index = 0;
 
   if (arr[0] > arr[1]) {
    temp = arr[0];
@@ -17,6 +18,23 @@ public class InsertionSort {
   for (int i=2; i<arr.length; i++) {
    for (int j=0; j<i; j++) {
     if (arr[i] < arr[j]) {
+     max = arr[j];
+     min = arr[i];
+     arr[j] = min;
+     index = j;
+    } else if (arr[i] < arr[j+1]) {
+     max = arr[j+1];
+     min = arr[i];
+     arr[j] = min;
+     index = j+1;
+    }
+    for (int k=arr.length-1; k>index; k--) {
+     temp = arr[k];
+     arr[k] = arr[k-1];
+     arr[k-1] = temp;
+     if (k == index+1) {
+      arr[k] = max;
+     }
     }
    }
   }
