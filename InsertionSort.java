f9b3cc51da90827d643f8baca3f588f0e8744db6
a8919779 <59424921+a8919779@users.noreply.github.com>
2021-02-05 12:28:59 +0900

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,7 +3,7 @@
 현재 인덱스를 앞에 위치한 값들과 비교하여 해당하는 구간에 삽입한다.
 */
 public class InsertionSort {
- public method main() {
+ public static void main(String[] args) {
   int[] arr = new int[]{1, 10, 5, 9, 8, 2, 4, 6, 3, 7};
   int j = 0;
   int temp = 0;
