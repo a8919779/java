c44633b428ca76f457b9871936cd5623a3d0d2ef
a8919779 <59424921+a8919779@users.noreply.github.com>
2021-02-05 12:28:03 +0900

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -5,38 +5,21 @@
 public class InsertionSort {
  public method main() {
   int[] arr = new int[]{1, 10, 5, 9, 8, 2, 4, 6, 3, 7};
-  int min = 0;
-  int max = 0;
+  int j = 0;
   int temp = 0;
-  int index = 0;
 
-  if (arr[0] > arr[1]) {
-   temp = arr[0];
-   arr[0] = arr[1];
-   arr[1] = temp;
-  }
-  for (int i=2; i<arr.length; i++) {
-   for (int j=0; j<i; j++) {
-    if (arr[i] < arr[j]) {
-     max = arr[j];
-     min = arr[i];
-     arr[j] = min;
-     index = j;
-    } else if (arr[i] < arr[j+1]) {
-     max = arr[j+1];
-     min = arr[i];
-     arr[j] = min;
-     index = j+1;
-    }
-    for (int k=arr.length-1; k>index; k--) {
-     temp = arr[k];
-     arr[k] = arr[k-1];
-     arr[k-1] = temp;
-     if (k == index+1) {
-      arr[k] = max;
-     }
-    }
+  for (int i=0; i<arr.length-1; i++) {
+   j = i;
+   while (arr[j] > arr[j+1]) {
+    temp = arr[j];
+    arr[j] = arr[j+1];
+    arr[j+1] = temp;
+    j--;
    }
+  }
+  
+  for (int k=0; k<arr.length; k++) {
+   System.out.println(arr[k]);
   }
  }
 }
